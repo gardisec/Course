d1745b55c5013d309dc36d11652be9ef78a16270
gardiex <gardiex@gardiex.myguest.virtualbox.org>
2024-12-25 03:16:03 +0700

    release 1.05

--- server/server.py
+++ server/server.py
@@ -725,5 +725,9 @@ def get_stores():
         conn.close()
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
